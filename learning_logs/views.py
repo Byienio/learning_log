e306f2177caabb6bd9ae93ec16dcce70dbdb6d21
jakub <bienio59@gmail.com>
2025-02-10 19:39:46 +0100

    edit entries and new accounts app

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm,EntryForm
 # Create your views here.
 
@@ -42,3 +42,16 @@ def new_entry(request, topic_id):
             return redirect('learning_logs:topic', topic_id=topic_id)
     contex = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', contex)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    contex = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', contex)
